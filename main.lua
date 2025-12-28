fc1d2710c4284ab9816d7f7f898d9e0b2a1b58a2
szx <trokanskypepa@gmail.com>
2025-12-28 01:39:52 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 -- Fivem FXAP Decryptor
 -- The ONLY **FREE** and working FXAP decryptor bot
 -- Unlimited decrypts, no limits
--- Discord: https://discord.gg/mtBPyEMUtE
+-- Discord: https://discord.gg/6C3P7mVkp3
 
